6d315e71ffa0cd834a2a02eb6c98ac72d4f5fe91
Brenda Guerra <102561007+Brenda-Guerra@users.noreply.github.com>
2023-08-25 20:24:34 -0300

    Create povoamento.sql

---
+++ CRIACAO/povoamento.sql
@@ -0,0 +1,33 @@
+--povoamento endereco
+
+INSERT INTO Endereco VALUES (endereco_tp('11111200', 'bairro arroz', 'rua feijao', 'numero 100', 'apto 200'));
+INSERT INTO Endereco VALUES (endereco_tp('22222300', 'bairro batata', 'rua cenoura', 'numero 300', 'apto 500'));
+INSERT INTO Endereco VALUES (endereco_tp('33333400', 'bairro abobrinha', 'rua espinafre', 'numero 200', 'apto 800'));
+INSERT INTO Endereco VALUES (endereco_tp('44444500', 'bairro couve', 'rua alface', 'numero 500', 'apto 1000'));
+INSERT INTO Endereco VALUES (endereco_tp('55555600', 'bairro tomate', 'rua cebola', 'numero 400', 'apto 1200'));
+INSERT INTO Endereco VALUES (endereco_tp('66666700', 'bairro pimentao', 'rua pimenta', 'numero 700', 'apto 1500'));
+INSERT INTO Endereco VALUES (endereco_tp('77777800', 'bairro alho', 'rua cebolinha', 'numero 600', 'apto 1800'));
+INSERT INTO Endereco VALUES (endereco_tp('88888900', 'bairro cenoura', 'rua beterraba', 'numero 900', 'apto 2000'));
+INSERT INTO Endereco VALUES (endereco_tp('99999000', 'bairro abacate', 'rua abacaxi', 'numero 800', 'apto 2200'));
+INSERT INTO Endereco VALUES (endereco_tp('10000000', 'bairro banana', 'rua maca', 'numero 1100', 'apto 2500'));
+INSERT INTO Endereco VALUES (endereco_tp('11112100', 'bairro uva', 'rua morango', 'numero 1000', 'apto 2800'));
+INSERT INTO Endereco VALUES (endereco_tp('11234600', 'bairro abacaxi', 'rua coco', 'numero 1300', 'apto 3100'));
+INSERT INTO Endereco VALUES (endereco_tp('12345800', 'bairro limao', 'rua laranja', 'numero 1200', 'apto 3300'));
+INSERT INTO Endereco VALUES (endereco_tp('13456900', 'bairro melancia', 'rua melao', 'numero 1500', 'apto 3600'));
+INSERT INTO Endereco VALUES (endereco_tp('14567800', 'bairro pitanga', 'rua jabuticaba', 'numero 1400', 'apto 3900'));
+INSERT INTO Endereco VALUES (endereco_tp('15678900', 'bairro manga', 'rua amora', 'numero 1700', 'apto 4200'));
+INSERT INTO Endereco VALUES (endereco_tp('16789000', 'bairro mamao', 'rua kiwi', 'numero 1600', 'apto 4500'));
+INSERT INTO Endereco VALUES (endereco_tp('17890100', 'bairro goiaba', 'rua framboesa', 'numero 1900', 'apto 4800'));
+INSERT INTO Endereco VALUES (endereco_tp('18901200', 'bairro caju', 'rua acerola', 'numero 1800', 'apto 5100'));
+INSERT INTO Endereco VALUES (endereco_tp('19012300', 'bairro figo', 'rua pera', 'numero 2100', 'apto 5400'));
+INSERT INTO Endereco VALUES (endereco_tp('20023400', 'bairro maracuja', 'rua abacaxi', 'numero 2000', 'apto 5700'));
+INSERT INTO Endereco VALUES (endereco_tp('21113500', 'bairro framboesa', 'rua morango', 'numero 2300', 'apto 6000'));
+INSERT INTO Endereco VALUES (endereco_tp('22223600', 'bairro kiwi', 'rua uva', 'numero 2200', 'apto 6300'));
+INSERT INTO Endereco VALUES (endereco_tp('23333700', 'bairro acerola', 'rua goiaba', 'numero 2500', 'apto 6600'));
+INSERT INTO Endereco VALUES (endereco_tp('24443800', 'bairro pera', 'rua caju', 'numero 2400', 'apto 6900'));
+INSERT INTO Endereco VALUES (endereco_tp('25553900', 'bairro abacaxi', 'rua maracuja', 'numero 2700', 'apto 7200'));
+INSERT INTO Endereco VALUES (endereco_tp('26664000', 'bairro morango', 'rua framboesa', 'numero 2600', 'apto 7500'));
+INSERT INTO Endereco VALUES (endereco_tp('27774100', 'bairro uva', 'rua kiwi', 'numero 2900', 'apto 7800'));
+INSERT INTO Endereco VALUES (endereco_tp('28884200', 'bairro goiaba', 'rua acerola', 'numero 2800', 'apto 8100'));
+INSERT INTO Endereco VALUES (endereco_tp('29994300', 'bairro caju', 'rua pera', 'numero 3100', 'apto 8400'));
+INSERT INTO Endereco VALUES (endereco_tp('30004400', 'bairro figo', 'rua maracuja', 'numero 3000', 'apto 8700'));
